de5bad7002c10fbc2eb4c7332b6e283685f21415
Douglas Urner <4495780+DouglasUrner@users.noreply.github.com>
2020-02-24 10:42:43 -0800

    Rewrite in ruby

--- assessment/auto-score.rb
+++ assessment/auto-score.rb
@@ -1,1 +1,17 @@
+#!/usr/bin/env ruby
 
+# Automated checks
+
+opts = {
+  tmp_dir: "tmp",
+  verbose: false
+}
+
+OptionParser.new do |o|
+  o.banner = "Usage: #{$0} [options]"
+  
+  o.on("-T TMPDIR") { |v| opts[:tmp_dir] = v }
+  o.on("-v")        { |v| opts[:verbose] = true }
+end.parse!
+
+p ARGV
